e18e65b5651d8cd44450c875550ab54ee2420b37
Renato Groffe <renato.groffe@yahoo.com.br>
2025-10-07 10:39:01 -0300

    Commit inicial

---
+++ src/ConsoleAppAzureSpeech/Inputs/InputHelper.cs
@@ -0,0 +1,20 @@
+﻿using Sharprompt;
+
+namespace ConsoleAppAzureSpeech.Inputs;
+
+public static class InputHelper
+{
+    public const string SIM = "Sim";
+    public const string NAO = "Nao";
+
+    public static bool ContinueWithTests()
+    {
+        var answer = Prompt.Select<string>(options =>
+        {
+            options.Message = "Continuar com novos testes?";
+            options.Items = [SIM, NAO];
+        });
+        Console.WriteLine();
+        return (answer == SIM);
+    }
+}
---
+++ src/ConsoleAppAzureSpeech/Program.cs
@@ -0,0 +1,61 @@
+﻿using ConsoleAppAzureSpeech.Inputs;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.Extensions.Configuration;
+using System.Globalization;
+
+Console.WriteLine("***** Testes com Azure Speech *****");
+Console.WriteLine();
+
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
+CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("pt-BR");
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+do
+{
+    await RecognizeSpeechAsync();
+    Console.WriteLine();
+} while (InputHelper.ContinueWithTests());
+
+async Task RecognizeSpeechAsync()
+{
+    var speechKey = configuration["AzureSpeech:ApiKey"];
+    var serviceRegion = configuration["AzureSpeech:ServiceRegion"];
+
+    var config = SpeechConfig.FromSubscription(speechKey, serviceRegion);
+    config.EnableAudioLogging();
+    config.SpeechRecognitionLanguage = "pt-BR";
+
+    using var recognizer = new SpeechRecognizer(config);
+    Console.WriteLine("Fale algo em português do Brasil...");
+    Console.WriteLine("Pressione ENTER para iniciar a captura de fala...");
+    Console.ReadLine();
+
+    var result = await recognizer.RecognizeOnceAsync();
+
+    switch (result.Reason)
+    {
+        case ResultReason.RecognizedSpeech:
+            Console.WriteLine($"Reconhecido: {result.Text}");
+            break;
+
+        case ResultReason.NoMatch:
+            Console.WriteLine("Não foi possível reconhecer fala.");
+            break;
+
+        case ResultReason.Canceled:
+            var cancellation = CancellationDetails.FromResult(result);
+            Console.WriteLine($"Reconhecimento cancelado. Motivo: {cancellation.Reason}");
+            if (cancellation.Reason == CancellationReason.Error)
+            {
+                Console.WriteLine($"Detalhes do erro: {cancellation.ErrorDetails}");
+            }
+            break;
+
+        default:
+            break;
+    }
+}
